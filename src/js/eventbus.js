006631919bea5c68e08e5d8ec4d41f005caec15b
Krzysztof Langner <klangner@gmail.com>
2013-10-30 12:18:42 +0100

    Event bus implemented

--- src/js/eventbus.js
+++ src/js/eventbus.js
@@ -8,6 +8,20 @@ function EventBus() {
 
 	var handlers = {}
 	
-	this.addEventListener = function(eventName, callback){
+	this.addEventListener = function(eventName, handler){
+		var eventHandlers = handlers[eventName] || [];
+		eventHandlers.push(handler);
+		handlers[eventName] = eventHandlers;
+	}
+	
+	this.sendEvent = function(eventName, eventData){
+		var eventHandlers = handlers[eventName] || [];
+		for(var i in eventHandlers){
+			eventHandlers[i](eventName, eventData);
+		}
+	}
+	
+	this.reset = function(){
+		handlers = {};
 	}
 }
--- tests/js/eventbus_tests.js
+++ tests/js/eventbus_tests.js
@@ -11,3 +11,43 @@ test( "Add handler", function() {
 	eventBus.addEventListener("TestEvent", function(eventName, eventData){});
 	ok( eventBus != null, "Can't create event bus!" );
 });
+
+test( "Add handler", function() {
+	var eventBus = new EventBus();
+	var receivedEvent = false;
+	eventBus.addEventListener("TestEvent", function(eventName, eventData){
+		receivedEvent = true;
+	});
+	eventBus.sendEvent('TestEvent', {});
+	ok( receivedEvent, "Event not received" );
+});
+
+test( "Event without handler", function() {
+	var eventBus = new EventBus();
+	eventBus.sendEvent('TestEvent', {});
+	ok( true, "Can't send event" );
+});
+
+test( "Add 2 handlers", function() {
+	var eventBus = new EventBus();
+	var eventCounter = 0;
+	eventBus.addEventListener("TestEvent", function(eventName, eventData){
+		eventCounter += 1;
+	});
+	eventBus.addEventListener("TestEvent", function(eventName, eventData){
+		eventCounter += 1;
+	});
+	eventBus.sendEvent('TestEvent', {});
+	ok( eventCounter == 2, "Expected 2 events. Received: " + eventCounter);
+});
+
+test( "Reset", function() {
+	var eventBus = new EventBus();
+	var receivedEvent = false;
+	eventBus.addEventListener("TestEvent", function(eventName, eventData){
+		receivedEvent = true;
+	});
+	eventBus.reset();
+	eventBus.sendEvent('TestEvent', {});
+	ok( !receivedEvent, "Event received after reset" );
+});
